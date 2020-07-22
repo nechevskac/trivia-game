2cf4488ea3bf2aad3f7f58c5f8a53c9f519f3d2b
nechevskac <cecanechevska.cn@gmail.com>
2020-07-22 21:00:04 +0200

    Add more dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-
-  return (
-    <>Trivia Game!</>
-  )
-}
+  return <>Trivia Game!</>;
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
